6b331ea72d0ce3a1249456ab9ae1d087c6d46939
Timothy Parrish <timothy_parrish@hotmail.com>
2021-04-12 18:00:40 -0400

    recursion completed through 4) is even

--- src/recursion.js
+++ src/recursion.js
@@ -45,6 +45,13 @@ var arraySum = function(array) {
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  if (Math.abs(n) === 0) {
+    return true;
+  } else if (Math.abs(n) === 1) {
+    return false;
+  } else {
+    return isEven(Math.abs(n) - 2);
+  }
 };
 
 // 5. Sum all integers below a given integer.
